1f01618fe3b8acb6379c4ce90f7424107d5030eb
LuzGarcia1620 <20223tn155@utez.edu.mx>
2025-07-09 10:09:24 -0600

    FIX: Cambios router/swagger

--- backend/src/modules/history/controller/history.controller.js
+++ backend/src/modules/history/controller/history.controller.js
@@ -1,4 +1,6 @@
 const historyService = require('../service/history.service')
+const { Router } = require('express')
+const routerHistory = Router()
 
 const getHistoriesByStudent = async (req, res) => {
     try {
@@ -12,6 +14,26 @@ const getHistoriesByStudent = async (req, res) => {
     }
 }
 
+routerHistory.get('/student/:studentId',
+    // #swagger.tags = ['Historial']
+    // #swagger.summary = 'Obtener historial por estudiante'
+    // #swagger.description = 'Devuelve todas las entrevistas simuladas realizadas por un estudiante específico.'
+    // #swagger.parameters['studentId'] = { description: 'ID del estudiante', in: 'path', required: true, type: 'string' }
+    // #swagger.responses[200] = {
+    //     description: 'Historial obtenido correctamente',
+    //     schema: [
+    //       {
+    //         simulator_id: "abc123",
+    //         date: "2025-07-08",
+    //         score: 87
+    //       }
+    //     ]
+    // }
+    // #swagger.responses[500] = {
+    //     description: 'Error interno del servidor'
+    // }
+    getHistoriesByStudent)
+
 module.exports = {
-    getHistoriesByStudent
+    routerHistory
 }
