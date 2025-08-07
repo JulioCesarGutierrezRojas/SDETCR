2f0b8b9211ae1ca742d80df94ca2fbce85bb048f
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-08-07 09:57:13 -0600

    HOTFIX: conflictos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,12 @@ import AppRouter from './router/router.jsx'
 import { SocketProvider } from './context/SocketContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
+createRoot(document.getElementById('root')).render( 
   <StrictMode>
     <AuthProvider>
-    <SocketProvider>
-      <AppRouter />
-    </SocketProvider>
-  </AuthProvider>
-    
+      <SocketProvider>
+        <AppRouter />
+      </SocketProvider>
+    </AuthProvider>   
   </StrictMode>,
 )
